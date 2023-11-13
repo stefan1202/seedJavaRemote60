9ac3a96b4308b4d6b046c96aa8561291953a5f13
StefanD <stefan.durla@gmail.com>
2023-11-13 20:55:58 +0200

    Initial commit

---
+++ src/main/java/ro/sda/seedjavaremote60/SeedJavaRemote60Application.java
@@ -0,0 +1,13 @@
+package ro.sda.seedjavaremote60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedJavaRemote60Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedJavaRemote60Application.class, args);
+    }
+
+}
---
+++ src/test/java/ro/sda/seedjavaremote60/SeedJavaRemote60ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.seedjavaremote60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedJavaRemote60ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
