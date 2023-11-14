d6177ea054b662c3d616f5160ac4d995648c0055
StefanD <stefan.durla@gmail.com>
2023-11-14 20:24:20 +0200

    Added a form validation

--- src/main/java/ro/sda/seedjavaremote60/controllers/HelloWorldController.java
+++ src/main/java/ro/sda/seedjavaremote60/controllers/HelloWorldController.java
@@ -1,21 +1,27 @@
 package ro.sda.seedjavaremote60.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import ro.sda.seedjavaremote60.models.Author;
+
 
 @Controller
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String helloWorld(@RequestParam(required = false) String name, Model model){
+    public String helloWorld(@RequestParam(required = false) String name,  Model model){
         model.addAttribute("name",name);
         model.addAttribute("formObject", new Author());
         return "hello";
     }
 
     @PostMapping("/process")
-    public String helloWorld(@ModelAttribute Author author, Model model){
+    public String helloWorld(@ModelAttribute("formObject") @Valid Author author, Errors errors, Model model){
+        if (errors.hasErrors()){
+            return "hello";
+        }
         model.addAttribute("author",author);
         return "display";
     }
--- src/main/java/ro/sda/seedjavaremote60/models/Author.java
+++ src/main/java/ro/sda/seedjavaremote60/models/Author.java
@@ -1,10 +1,19 @@
 package ro.sda.seedjavaremote60.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
 
 @Data
 public class Author {
+    @NotNull
+    @Length(min = 3,max = 50, message = "Name must contain between 3 and 50 characters")
     private String name;
+    @NotNull
+    @Length(min = 3,max = 50, message = "LastName must contain between 3 and 50 characters")
     private String lastName;
+    @Min(value =18,message = "Age must be higher than 18")
     private int age;
 }
