fe2872dafb06e761dc5c64153d88bf4efd22d739
StefanD <stefan.durla@gmail.com>
2023-11-14 19:00:06 +0200

    Added first view

--- src/main/java/ro/sda/seedjavaremote60/HelloWorldController.java
+++ src/main/java/ro/sda/seedjavaremote60/HelloWorldController.java
@@ -1,12 +1,16 @@
 package ro.sda.seedjavaremote60;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String helloWorld(){
+    public String helloWorld(@RequestParam(required = false) String name, Model model){
+        model.addAttribute("time",System.currentTimeMillis());
+        model.addAttribute("name",name);
         return "hello";
     }
 }
