4cee149c233870a2306d47f6ea1a79f438ae9dbf
StefanD <stefan.durla@gmail.com>
2023-11-16 20:40:20 +0200

    Added edit for an entry

--- src/main/java/ro/sda/seedjavaremote60/repositories/AuthorsRepository.java
+++ src/main/java/ro/sda/seedjavaremote60/repositories/AuthorsRepository.java
@@ -1,6 +1,8 @@
 package ro.sda.seedjavaremote60.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ro.sda.seedjavaremote60.entities.AuthorEntity;
 
 import java.util.List;
@@ -8,4 +10,9 @@ import java.util.List;
 public interface AuthorsRepository extends JpaRepository<AuthorEntity, Long> {
 
     List<AuthorEntity> findAllByNameStartingWith(String name);
+
+    List<AuthorEntity> findTop3ByNameStartingWith(String name);
+
+    @Query(value="Select a from authors a where a.varsta>:age", nativeQuery = false)
+    List<AuthorEntity> giveAllAuthorsByAge(@Param("age") int paramAge );
 }
