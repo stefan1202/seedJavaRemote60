85e25b859dcdaa2a643ff26fb63af7b6b37c78f4
StefanD <stefan.durla@gmail.com>
2023-11-21 19:31:11 +0200

    Added configuration for security

--- src/main/java/ro/sda/seedjavaremote60/configuration/SecurityConfiguration.java
+++ src/main/java/ro/sda/seedjavaremote60/configuration/SecurityConfiguration.java
@@ -3,10 +3,15 @@ package ro.sda.seedjavaremote60.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
 
 @Component
 public class SecurityConfiguration {
@@ -20,7 +25,15 @@ public class SecurityConfiguration {
                     .requestMatchers(mvcMatcherBuilder.pattern("/h2**")).permitAll()
                     .requestMatchers(mvcMatcherBuilder.pattern("/login")).permitAll()
                     .requestMatchers(mvcMatcherBuilder.pattern("/logout")).permitAll()
-                    .anyRequest().authenticated();
+                    .requestMatchers(mvcMatcherBuilder.pattern("/author/edit"),
+                            mvcMatcherBuilder.pattern("/author/create"),
+                            mvcMatcherBuilder.pattern("/author/edit")
+                            ).hasRole("ADMIN")
+                    .requestMatchers(mvcMatcherBuilder.pattern("/book/**")
+                    ).hasRole("USER")
+                    .requestMatchers(mvcMatcherBuilder.pattern("/author/list")).authenticated()
+//                    .anyRequest().authenticated()
+            ;
 
         });
         http.formLogin((loginForm)->loginForm.permitAll().defaultSuccessUrl("/author/list").successForwardUrl("/author/list"));
@@ -29,5 +42,16 @@ public class SecurityConfiguration {
         http.cors(AbstractHttpConfigurer::disable); //disable cors (.cors((cors) -> cors.disable()))
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails userAdmin = User.withDefaultPasswordEncoder().username("vlad").password("root").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(List.of(user,userAdmin));
     }
 }
