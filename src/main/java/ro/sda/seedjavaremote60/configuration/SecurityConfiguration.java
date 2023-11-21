a630dceaca9acb746d9aa3ccc1f971f3ce851e00
StefanD <stefan.durla@gmail.com>
2023-11-21 20:10:03 +0200

    Added configuration for security

--- src/main/java/ro/sda/seedjavaremote60/configuration/SecurityConfiguration.java
+++ src/main/java/ro/sda/seedjavaremote60/configuration/SecurityConfiguration.java
@@ -1,13 +1,14 @@
 package ro.sda.seedjavaremote60.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
@@ -18,25 +19,25 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector).servletPath("/");
+//        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector).servletPath("/");
         http.authorizeHttpRequests((req) ->{
 
-            req.requestMatchers(mvcMatcherBuilder.pattern("/api/**")).permitAll()
-                    .requestMatchers(mvcMatcherBuilder.pattern("/h2**")).permitAll()
-                    .requestMatchers(mvcMatcherBuilder.pattern("/login")).permitAll()
-                    .requestMatchers(mvcMatcherBuilder.pattern("/logout")).permitAll()
-                    .requestMatchers(mvcMatcherBuilder.pattern("/author/edit"),
-                            mvcMatcherBuilder.pattern("/author/create"),
-                            mvcMatcherBuilder.pattern("/author/edit")
-                            ).hasRole("ADMIN")
-                    .requestMatchers(mvcMatcherBuilder.pattern("/book/**")
-                    ).hasRole("USER")
-                    .requestMatchers(mvcMatcherBuilder.pattern("/author/list")).authenticated()
-//                    .anyRequest().authenticated()
+            req.requestMatchers("/api/**").permitAll()
+//                    .requestMatchers(("/h2**")).permitAll()
+                    .requestMatchers("/login").permitAll()
+                    .requestMatchers(("/logout")).permitAll()
+                    .requestMatchers(("/author/edit**"),
+                            ("/author/create"),
+                            ("/author/edit")
+                            ).hasAuthority("ROLE_author")
+                    .requestMatchers(("/book/**")
+                    ).hasRole("book")
+                    .requestMatchers(("/author/list")).authenticated()
+                    .anyRequest().authenticated()
             ;
 
         });
-        http.formLogin((loginForm)->loginForm.permitAll().defaultSuccessUrl("/author/list").successForwardUrl("/author/list"));
+        http.formLogin().permitAll();
         http.logout((logout)->logout.permitAll().clearAuthentication(true).invalidateHttpSession(true));
         http.csrf(AbstractHttpConfigurer::disable); //disable csrf (.csrf((csrf) -> csrf.disable()))
         http.cors(AbstractHttpConfigurer::disable); //disable cors (.cors((cors) -> cors.disable()))
@@ -49,9 +50,14 @@ public class SecurityConfiguration {
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("password")
-                .roles("USER")
+                .roles("book")
                 .build();
-        UserDetails userAdmin = User.withDefaultPasswordEncoder().username("vlad").password("root").roles("ADMIN").build();
+        UserDetails userAdmin = User.withDefaultPasswordEncoder().username("vlad").password("root").roles("author").build();
         return new InMemoryUserDetailsManager(List.of(user,userAdmin));
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
